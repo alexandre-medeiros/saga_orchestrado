e3ef236b1e5990bb9c7158aed68329a0ae57b315
Alexandre Martins de Medeiros <alexandre.medmax@gmail.com>
2024-02-17 11:32:34 -0300

    initial config

---
+++ payment-service/src/test/java/br/com/himax/saga/orchestration/inventory/OrchestradoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.himax.saga.orchestration.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
